8c92f9892035d827a8944d67f3d63154a404f9c7
Hanna <46329108+HannHop@users.noreply.github.com>
2021-10-13 21:37:05 +0200

    Update turtle_lab.py

--- turtle_lab.py
+++ turtle_lab.py
@@ -3,13 +3,13 @@ import turtle
 tur = turtle.Turtle()
 tur.speed('fastest')
 
-
+# the turtle will go in anti-clockwise direction starting from its default beginning
 def fractal_square(x):
  if x<=1:
   return
  tur.forward(x) # go x
  tur.left(90) # turn by 90 deg angle
- tur.forward(x) # go x right
+ tur.forward(x) # go x left
  tur.left(90) # turn d
  tur.forward(x) # go x down
  tur.left(90) # turn left
@@ -22,15 +22,12 @@ fractal_square(200)
 def fractal_triangle(x):
  if x<=1:
   return
- tur.forward(x) # go x
+ tur.forward(x) 
  tur.left(120)
- tur.forward(x) # go x right
- tur.left(120) # turn d
- tur.forward(x) # go x down
- tur.left(120) # turn left
- #tur.forward(x) # go to the beginning pointp
- #tur.left(120)
- #tur.right(90)
+ tur.forward(x) 
+ tur.left(120) 
+ tur.forward(x)
+ tur.left(120)
  fractal_triangle(0.9 * x)
 
 fractal_triangle(300)
